dfbc03977b4ef2b86bd4b4712ac3d82920e941db
Reetu Mutti <reetu.mutti@pulseenergy.com>
2014-08-08 16:25:50 -0700

    Add typedef examples

--- HelloObjectiveC/main.m
+++ HelloObjectiveC/main.m
@@ -7,23 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+
+// Macros
 #define PI 3.14159
 #define RAD_TO_DEG(radians) (radians * 180.0 / PI)
 
+// Basic TypeDefs
+typedef unsigned char ColorComponent;
+typedef int KmPerHr;
+typedef int Hour;
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
         
-        double odom = 9008.45;
-        int odomInt = (int)odom;
+        double odometer = 9008.45;
+        int odometerInt = (int)odometer;
         
-        NSLog(@"You've driven %.2f km (%i)", odom, odomInt);
+        NSLog(@"You've driven %.2f km (%i)", odometer, odometerInt);
         NSLog(@"7 / 2 is %d remainder %d, or %f", 7/2, 7%2, 7/2.0);
         
         double angle = PI / 2;
-        NSLog(@"%f", RAD_TO_DEG(angle));
+        NSLog(@"%.3f", RAD_TO_DEG(angle));
+        
+        ColorComponent red = 255;
+        ColorComponent green = 160;
+        ColorComponent blue = 0;
+        NSLog(@"This is your colour: (R: %hhu, G:%hhu, B:%hhu)", red, green, blue);
+        
+        KmPerHr currentSpeed = 150;
+        Hour journeyDuration = 3;
+        NSLog(@"You traveled at %dkm/h for %d hours. Total distance traveled is %dkm.", currentSpeed, journeyDuration, currentSpeed*journeyDuration);
+        
         
         
         
